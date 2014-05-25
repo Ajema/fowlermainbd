de3c0163cce09d76f1109abdea862625b872422e
Bastian Donat <geabw@yahoo.de>
2014-05-25 12:02:02 +0200

    Refactoring "amountFor" from Customer to Rental and renaming to "getCharge"

--- src/Customer.java
+++ src/Customer.java
@@ -25,7 +25,7 @@ class Customer {
         while (rentals.hasMoreElements()) {
             double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
-            thisAmount = amountFor(each);
+            thisAmount = each.amountFor();
 // add frequent renter points
             frequentRenterPoints++;
 // add bonus for a two day new release rental
@@ -44,23 +44,4 @@ class Customer {
         return result;
     }
 
-    private double amountFor(Rental aRental) {
-        double result = 0;
-        switch (aRental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (aRental.getDaysRented() > 2)
-                    result += (aRental.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += aRental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (aRental.getDaysRented() > 3)
-                    result += (aRental.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return result;
-    }
 }
